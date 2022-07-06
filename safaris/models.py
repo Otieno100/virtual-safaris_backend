c83411f4d0bec9213012f1d25ea90c4b88e2fa0b
albright <sungutialbright@gmail.com>
2022-07-06 21:56:45 +0300

    add payment model

--- safaris/models.py
+++ safaris/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    # user = models.ForeignKey( Profile, blank=True , null=True, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=100)
+    phone_number =models.PositiveIntegerField(default=254799735661)
+
+    def __str__(self):
+        return f'{self.name} payment'
+
+    def save_payment(self):
+        self.save()
