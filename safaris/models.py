8e38de1b9b6675b1f2de61aabcf8c2c1c1111084
Otieno100 <brian.masafu@student.moringaschool.com>
2022-07-06 21:33:16 +0300

    created views for the safaris model

--- safaris/models.py
+++ safaris/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Create your models here.
+
+class Safaris(models.Model):
+    """
+    A class thaat determines how photos will be saved into the database
+    """
+    name = models.CharField(max_length=344)
+    description = models.TextField()
+    location = models.CharField(max_length=400)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('safais-image', null=True)
+    video = models.FileField(upload_to ="video/%y")
+    
+
+    def __str__(self):
+      return self.name
+
+
--- safaris/views.py
+++ safaris/views.py
@@ -1,3 +1,47 @@
+from urllib import request
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  Safaris
+from .serializer import SafarisSerializer
+from rest_framework import status
 
 # Create your views here.
+
+
+class HelloKenya(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        content = {
+            'message':"HelloKenya"
+        }
+        return Response(content)
+    
+class Extractor(APIView):
+    parser_classes = (IsAuthenticated,)
+    def get(self, request):
+        content = {
+                'username':request.user.username,
+                'password': request.user.password,
+            }
+        return Response(content)
+
+
+
+class safaris(APIView):
+    def get(self, request, format=None):
+        safaris = Safaris.objects.all()
+        serializers = SafarisSerializer(safaris, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = SafarisSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
